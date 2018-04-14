bd7b9b6b1a4bc32096bcf0ef077bd4819df118c8
Guillaume Pinot <texitoi@texitoi.eu>
2018-04-14 22:19:18 +0200

    support ID search

---
+++ src/eval.rs
@@ -0,0 +1,84 @@
+use ntm;
+use expr;
+use ntm::collection::{Collection, CollectionWithId, Id};
+use ntm::relations::IdxSet;
+
+macro_rules! dispatch {
+    ($model:expr, $obj:expr, $expr:expr) => {
+        match $obj {
+            "contributor" => $expr ( & $model.contributors ),
+            "dataset" => $expr ( & $model.datasets ),
+            "network" => $expr ( & $model.networks ),
+            "commercial_mode" => $expr ( & $model.commercial_modes ),
+            "line" => $expr ( & $model.lines ),
+            "route" => $expr ( & $model.routes ),
+            "vehicle_journey" => $expr ( & $model.vehicle_journeys ),
+            "physical_mode" => $expr ( & $model.physical_modes ),
+            "stop_area" => $expr ( & $model.stop_areas ),
+            "company" => $expr ( & $model.companies ),
+            _ => Default::default(),
+        }
+    }
+}
+
+pub struct Eval<'a, T: 'a> {
+    model: &'a ntm::Model,
+    target: &'a Collection<T>,
+}
+impl<'a, T> Eval<'a, T>
+where
+    IdxSet<ntm::objects::Contributor>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::Dataset>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::Network>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::CommercialMode>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::Line>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::Route>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::VehicleJourney>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::PhysicalMode>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::StopArea>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::StopPoint>: ntm::model::GetCorresponding<T>,
+    IdxSet<ntm::objects::Company>: ntm::model::GetCorresponding<T>,
+{
+    pub fn new(target: &'a Collection<T>, model: &'a ntm::Model) -> Self {
+        Eval { target, model }
+    }
+    pub fn run(&self, e: &expr::Expr) -> IdxSet<T> {
+        self.expr(e)
+    }
+    fn all(&self) -> IdxSet<T> {
+        self.target.iter().map(|o| o.0).collect()
+    }
+    fn expr(&self, e: &expr::Expr) -> IdxSet<T> {
+        use expr::Expr::*;
+        match e {
+            Pred(p) => self.pred(p),
+            And(l, r) => &self.expr(l) & &self.expr(r),
+            Or(l, r) => &self.expr(l) | &self.expr(r),
+            Diff(l, r) => &self.expr(l) - &self.expr(r),
+        }
+    }
+    fn pred(&self, p: &expr::Pred) -> IdxSet<T> {
+        use expr::Pred::*;
+        match p {
+            All => self.all(),
+            Empty => IdxSet::default(),
+            Fun(f) => self.fun(f),
+        }
+    }
+    fn fun(&self, f: &expr::Fun) -> IdxSet<T> {
+        match (f.method.as_str(), f.args.as_slice()) {
+            ("id", [arg]) | ("uri", [arg]) => self.id(&f.obj, arg),
+            _ => unimplemented!(),
+        }
+    }
+    fn id(&self, obj: &str, id: &str) -> IdxSet<T> {
+        dispatch!(self.model, obj, |c| self.id_impl(c, id))
+    }
+    fn id_impl<U: Id<U>>(&self, objs: &CollectionWithId<U>, id: &str) -> IdxSet<T>
+    where
+        IdxSet<U>: ntm::model::GetCorresponding<T>,
+    {
+        let from = objs.get_idx(id).into_iter().collect();
+        self.model.get_corresponding(&from)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ extern crate combine;
 #[macro_use]
 extern crate structopt;
 extern crate failure;
-extern crate navitia_model;
+extern crate navitia_model as ntm;
 
-use navitia_model as ntm;
-use ntm::collection::{Collection, CollectionWithId, Id};
+use ntm::collection::{Collection, Id};
 use ntm::relations::IdxSet;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 pub mod expr;
+pub mod eval;
 
 fn main() {
     if let Err(err) = run(Opt::from_args()) {
@@ -30,13 +30,16 @@ struct Opt {
 }
 
 fn run(opt: Opt) -> Result<(), failure::Error> {
+    print!("Reading NTFS...");
+    io::stdout().flush()?;
     let model = ntm::ntfs::read(&opt.ntfs)?;
+    println!(" done.");
     let stdin = io::BufReader::new(io::stdin());
     for cmd in stdin.lines() {
         let cmd = cmd?;
         match expr::Expr::parse(cmd.as_str()) {
             Ok(expr) => print_ids(
-                &Eval::new(&model.stop_areas, &model).expr(&expr),
+                &eval::Eval::new(&model.stop_areas, &model).run(&expr),
                 &model.stop_areas,
             ),
             Err(e) => eprintln!("{}", e),
@@ -51,58 +54,4 @@ fn print_ids<T: Id<T>>(set: &IdxSet<T>, objects: &Collection<T>) {
         print!("{},", objects[idx].id());
     }
     println!();
-}
-
-struct Eval<'a, T: 'a> {
-    model: &'a ntm::Model,
-    target: &'a Collection<T>,
-}
-impl<'a, T> Eval<'a, T>
-where
-    IdxSet<ntm::objects::Line>: ntm::model::GetCorresponding<T>,
-    IdxSet<ntm::objects::StopArea>: ntm::model::GetCorresponding<T>,
-{
-    fn new(target: &'a Collection<T>, model: &'a ntm::Model) -> Self {
-        Eval { target, model }
-    }
-    fn all(&self) -> IdxSet<T> {
-        self.target.iter().map(|o| o.0).collect()
-    }
-    fn expr(&self, e: &expr::Expr) -> IdxSet<T> {
-        use expr::Expr::*;
-        match e {
-            Pred(p) => self.pred(p),
-            And(l, r) => &self.expr(l) & &self.expr(r),
-            Or(l, r) => &self.expr(l) | &self.expr(r),
-            Diff(l, r) => &self.expr(l) - &self.expr(r),
-        }
-    }
-    fn pred(&self, p: &expr::Pred) -> IdxSet<T> {
-        use expr::Pred::*;
-        match p {
-            All => self.all(),
-            Empty => IdxSet::default(),
-            Fun(f) => self.fun(f),
-        }
-    }
-    fn fun(&self, f: &expr::Fun) -> IdxSet<T> {
-        match (f.method.as_str(), f.args.as_slice()) {
-            ("id", [arg]) | ("uri", [arg]) => self.id(&f.obj, arg),
-            _ => unimplemented!(),
-        }
-    }
-    fn id(&self, obj: &str, id: &str) -> IdxSet<T> {
-        match obj {
-            "line" => self.id_impl(&self.model.lines, id),
-            "stop_area" => self.id_impl(&self.model.stop_areas, id),
-            _ => unimplemented!(),
-        }
-    }
-    fn id_impl<U: Id<U>>(&self, objs: &CollectionWithId<U>, id: &str) -> IdxSet<T>
-    where
-        IdxSet<U>: ntm::model::GetCorresponding<T>,
-    {
-        let from = objs.get_idx(id).into_iter().collect();
-        self.model.get_corresponding(&from)
-    }
 }
